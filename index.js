553e5f20be83c50082452ff772a810b9ef8f981c
Bengt Brodersen <bengt.brodersen@gmail.com>
2024-05-07 09:58:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ document.head.insertAdjacentHTML(
         grid-template-columns: 0; /* hide login header */
         grid-template-rows: 0 1fr 30px; /* hide organization navbar */
     }
-    #root > .app > div > .grid .\\[grid-area\\:Statusbar\\] > div:last-child > button {
+    #root > .app > div > .grid .\\[grid-area\\:Statusbar\\] > div > div:last-child > button {
         display: none; /* hide footer login button */
     }
-    #wrapper > aside > div > div:first-child > ol > li:first-child {
-        visibility: collapse; /* hide sidebar login button */
+    #sidebar > div > div:first-child > ol > li:first-child {
+        display: none; /* hide sidebar login button */
     }
 </style>`
 )
