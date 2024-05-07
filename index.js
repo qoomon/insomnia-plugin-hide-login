c98e0d57b962e6d92f97524fa4d6ae0814f4c61f
Bengt Brodersen <bengt.brodersen@gmail.com>
2024-05-07 10:33:36 +0200

    fix: hide organization button in footer

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ document.head.insertAdjacentHTML(
         grid-template-rows: 0 1fr 30px; /* hide organization navbar */
     }
     #root > .app > div > .grid .\\[grid-area\\:Statusbar\\] > div > div:last-child > button {
-        display: none; /* hide footer login button */
+        display: none; /* hide footer organizations sidebar button */
+    }
+    #root > .app > div > .grid .\\[grid-area\\:Statusbar\\] > button {
+      display: none; /* hide footer login button */
     }
     #sidebar > div > div:first-child > ol > li:first-child {
         display: none; /* hide sidebar login button */
     }
 </style>`
 )
+
