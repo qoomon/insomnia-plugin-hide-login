e272c85ae1e40c1cc9e941f236844ab0bd6f9e01
Bengt Brodersen <bengt.brodersen@gmail.com>
2024-05-17 15:33:26 +0200

    refactor

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This plugin hides all login-related UI elements to create a cleaner look for tho
 ## Install
 - Go to [Plugin Hub](https://insomnia.rest/plugins/insomnia-plugin-hide-login)
 - Click `Install Plugin` button
+
+## Development
+### Release
+- npm release
--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-const styleId = 'hide-login-plugin'
-document.getElementById(styleId)?.remove()
-document.head.insertAdjacentHTML(
-    'beforeend',
-    `<style id="${styleId}">
-    #root > .app > div > .grid {
-        grid-template-columns: 0; /* hide login header */
-        grid-template-rows: 0 1fr 30px; /* hide organization navbar */
-    }
-    #root > .app > div > .grid .\\[grid-area\\:Statusbar\\] > div > div:last-child > button {
-        display: none; /* hide footer organizations sidebar button */
-    }
-    #root > .app > div > .grid .\\[grid-area\\:Statusbar\\] > button {
-      display: none; /* hide footer login button */
-    }
-    #sidebar > div > div:first-child > ol > li:first-child {
-        display: none; /* hide sidebar login button */
-    }
-</style>`
-)
+const sheet = new CSSStyleSheet();
+sheet.id = 'insomnia-plugin-hide-login';
+sheet.replaceSync(`
+  /* Clean-up Header */
+  #root .\\[grid-area\\:Header\\] div:has( > span > a[href='https://github.com/Kong/insomnia/stargazers']),
+  #root .\\[grid-area\\:Header\\] div > a[href='/auth/login'] {
+    display: none !important;
+  }
+  
+  /* Clean-up Navbar */
+  #root .grid-template-app-layout.with-navbar {
+    grid-template-areas:
+      "Header Header"
+      "Content Content"
+      "Statusbar Statusbar" !important;
+  }
+  #root .\\[grid-area\\:Navbar\\] {
+    display: none !important;
+  }
+  
+  /* Clean-up Sidebar */
+  #root #sidebar > div > div:first-child > ol > li:has([data-testid='project']) {
+    display: none !important;
+  }
+      
+  /* Clean-up Statusbar */
+  #root .\\[grid-area\\:Statusbar\\] > button,
+  #root .\\[grid-area\\:Statusbar\\] > div > div:last-child > button  {
+    display: none !important;
+  }
+`);
 
+// remove previous style sheet
+document.adoptedStyleSheets = document.adoptedStyleSheets
+  ?.filter(it => it.id !== sheet.id) ?? [];
+
+// apply style sheet
+document.adoptedStyleSheets.push(sheet);
